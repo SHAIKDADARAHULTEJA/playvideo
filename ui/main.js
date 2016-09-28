f2457522e9dce86643fe0e1d3f53dfbc5573db19
SHAIKDADARAHULTEJA <SHAIKDADARAHULTEJA@gmail.com>
2016-09-28 15:03:10 +0530

    added javascript

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+//change the text of maintext div
+var element = document.getElementById('main-text');
+element.innerHTML = "new Value";
+//move the image to the Right continuosly
+var img  = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight()
+{
+	marginLeft = marginLeft + 1;
+	img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function()
+{
+	var interval = setInterval(moveRight,50);
+}
