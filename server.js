5fa5aeee2a6f01d70b9daed9456040ba6cd7df36
SHAIKDADARAHULTEJA <SHAIKDADARAHULTEJA@gmail.com>
2016-09-28 14:05:54 +0530

    reduced html and javascript

--- server.js
+++ server.js
@@ -4,31 +4,102 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles = {
+'article-one' : {
+title :'Article One | Shaik Rahul',
+heading: 'Article One',
+date   : new Date(),
+content: 
+`<p>
+      This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+  </p>
+   <p>
+      This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+  </p>
+   <p>
+      This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+  </p>`
+},
+'article-two'  : {
+title :'Article Two | Shaik Rahul',
+heading: 'Article Two',
+date   : new Date(),
+content: 
+`
+<p>This is the content for my second article.
+</p>
+`},
+'article-three':{
+title :'Article Three | Shaik Rahul',
+heading: 'Article Three',
+date   : new Date(),
+content: 
+`
+<p>This is the content for my Third article.
+</p>
+`
+}
 
+};
+function createTemplate(data){
+var title = data.title;
+var date  = data.date;
+var heading = data.heading;
+var content = data.content;
+var htmltemplate  = `<html>
+  <head>
+      <title>
+         ${title}
+      </title>
+      <meta name="viewport"  content="width-device-width,initial-scale=1" />
+      <link href="/ui/style.css" rel="stylesheet" />
+     </head>
+  <body>
+      <div class="container">
+      <div>
+          <a href="/"> <font color="white">Home</font></a>
+      </div>
+      <hr/>
+      <h3>
+          ${heading}
+      </h3>
+      <div>
+${date}
+</div>
+<div>
+${content} 
+</div> 
+</div>
+</body>
+</html>
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName',function(req,res){
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two',function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+console.log(`IMAD course app listening on port ${port}!`);
 });
