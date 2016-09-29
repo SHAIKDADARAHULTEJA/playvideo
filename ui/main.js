ac5015d0ec74d15c33245c01cb776166c861626e
SHAIKDADARAHULTEJA <SHAIKDADARAHULTEJA@gmail.com>
2016-09-29 21:59:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,18 +21,36 @@ request.onreadystatechange = function()
 request.open('GET','http://shaikdadarahulteja.imad.hasura-app.io/counter',true);
 request.send(null);
 };
-var Inputvalue = document.getElementById('name');
-var name = Inputvalue.value;
+
 
 var submit = document.getElementById('submit-btn');
 submit.onclick = function()
 {
-  var names = ['name1','name2','name3'];
-  var list = '';
-  for(var i=0;i<names.length;i++)
-  {
+    
+     var  request = new XMLHttpRequest();
+//Capture the response and store it in a variable
+request.onreadystatechange = function()
+{
+ if(request.readyState == XMLHttpRequest.DONE)
+ {
+ 	if(request.status == 200)
+ 	{
+ 
+      var names = request.responseText;
+      names = JSON.parse(names);
+      var list = '';
+       for(var i=0;i<names.length;i++)
+      {
       list += '<li>'+names[i]+'</li>';
-  }
-    var ul = document.getElementById('nameslist');
-    ul.innerHTML = list;
+      }
+     var ul = document.getElementById('nameslist');
+     ul.innerHTML = list;
+ 	}
+ }
+};
+	var Inputvalue = document.getElementById('name');
+    var name = Inputvalue.value;
+    request.open('GET','http://shaikdadarahulteja.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+
 };
