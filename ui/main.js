3b0cb85537ac0a77dabfe4654b3f63cdbbb93124
SHAIKDADARAHULTEJA <SHAIKDADARAHULTEJA@gmail.com>
2016-09-29 15:44:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,26 +3,26 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function()
 {
-	//Create a request to the counter endpoint
-     var  request = new XMLHttpRequest();
-	//Capture the response and store it in a variable
- request.onreadystatechange = function()
+//Create a request to the counter endpoint
+ var  request = new XMLHttpRequest();
+//Capture the response and store it in a variable
+request.onreadystatechange = function()
+{
+ if(request.readyState == XMLHttpRequest.DONE)
  {
- 	if(request.readyState == XMLHttpRequest.DONE)
+ 	if(request.status == 200)
  	{
- 		if(request.status == 200)
- 		{
- 			var counter = request.responseText;
- 			var span = document.getElementById('count');
-	span.innerHTML = counter.toString();
- 		}
+ 		var counter = request.responseText;
+ 		var span = document.getElementById('count');
+    	span.innerHTML = counter.toString();
  	}
- };
-	//Render the variable in the correct span
-	// counter = counter + 1;
-	// var span = document.getElementById('count');
-	// span.innerHTML = counter.toString();
-  //Make a request
-  request.open('GET','http://shaikdadarahulteja.imad.hasura-app.io/counter',true);
-  request.send(null);
+ }
+};
+//Render the variable in the correct span
+// counter = counter + 1;
+// var span = document.getElementById('count');
+// span.innerHTML = counter.toString();
+//Make a request
+request.open('GET','http://shaikdadarahulteja.imad.hasura-app.io/counter',true);
+request.send(null);
 }
