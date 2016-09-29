e39d62b35387ca9a9582dd880bb311469a92ad9f
SHAIKDADARAHULTEJA <SHAIKDADARAHULTEJA@gmail.com>
2016-09-29 21:28:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,18 @@ request.onreadystatechange = function()
 request.open('GET','http://shaikdadarahulteja.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+var Inputvalue = document.getElementById('name');
+var name = Inputvalue.value;
+
+var submit = document.getElementById('submit-btn');
+submit.onclick = function()
+{
+  var names = ['name1','name2','name'];
+  var list = '';
+  for(var i=0;i<names.length;i++)
+  {
+      list += '<li>'+names[i]+'</li>';
+  }
+    var ul = document.getElementById('nameslist');
+    ul.innerHTML = list;
+};
